e518903d74aa0a0ce5a772cab69a4e37c05dca36
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2017-04-17 12:16:52 -0700

    refact: use direct POST instead of clicks
    Also throttle (limit 100ms)
    Also hide the message immediately

--- index.js
+++ index.js
@@ -6,23 +6,58 @@
 
 (function() {
   'use strict';
-  const deleteHoveredMessage = () => {
-    const actionsBtn = document.querySelector('ts-message:hover button[data-action="actions_menu"]')
-    if (!actionsBtn) return
-    actionsBtn.click()
-    const deleteBtn = document.querySelector('li#delete_link a')
-    if (!deleteBtn) return
-    deleteBtn.click()
-    const okBtn = document.querySelector('button.btn.dialog_go.btn_danger')
-    if (!okBtn) return
-    okBtn.click()
-  }
+  const _x_id = () => `${window.boot_data.version_uid.substring(0, 8)}-${(new Date).getTime()/1000.0}`
 
-  const keyUp = e => {
-    // Control + Shift + Space
-    if (e.ctrlKey && e.shiftKey && e.keyCode == '32') {
-      deleteHoveredMessage()
+  // Limit is in ms
+  const throttle = (callback, limit = 100) => {
+    var wait = false
+    return function() { // We return a throttled function
+      if (!wait) {
+        callback.call()
+        wait = true // Prevent future invocations for a time
+        setTimeout(function() {
+          wait = false
+        }, limit)
+      }
     }
   }
-  document.addEventListener('keyup', keyUp, false)
+
+  const queryString = obj => {
+    var str = [];
+    for (var p in obj)
+      if (obj.hasOwnProperty(p)) {
+        str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
+      }
+    return str.join('&')
+  }
+
+  const deleteHoveredMessage = () => {
+    const el = document.querySelector('ts-message:hover')
+    el.style.display = "none"
+    const ts = el.getAttribute('data-ts')
+    const channel = el.getAttribute('data-model-ob-id')
+    const token = window.boot_data.api_token
+    const url = `https://${window.location.host}/api/chat.delete?_x_id=${_x_id()}`
+    const params = queryString({
+      channel,
+      token,
+      ts
+    })
+
+    const http = new XMLHttpRequest()
+    http.open("POST", url, true)
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+    http.send(params)
+  }
+
+  const throttledDelete = throttle(deleteHoveredMessage)
+
+  const handleKeyUp = e => {
+    // Shift + Backspace
+    if (e.shiftKey && e.keyCode === 8) {
+      throttledDelete()
+    }
+  }
+
+  document.addEventListener('keyup', handleKeyUp, false)
 })();
